03fc3715ed4cd63bd2fd64a43f120ef6c8658023
Sean Suzuki <suzatsuhiro@gmail.com>
2023-01-19 00:02:08 -0800

    Updated about page.

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,16 +4,10 @@ import { Main } from '@/templates/Main';
 const About = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
+      Hello, I just created this website using a boilerplate code provided by
+      https://github.com/ixartz/Next-js-Boilerplate. I will be adding projects
+      that I have/will be working on as well as a quick tutorial on how it was
+      built from time to time.
     </p>
   </Main>
 );
