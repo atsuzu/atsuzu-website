c7f7621a7da4afe037b3c973094307b9f9c79f3a
Sean Suzuki <suzatsuhiro@gmail.com>
2023-01-18 21:20:26 -0800

    rattier.

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -1,11 +1,18 @@
-import { Meta } from '@/layouts/Meta';
-import { Main } from '@/templates/Main';
+import { Meta } from "@/layouts/Meta";
+import { Main } from "@/templates/Main";
 
 const PrivacyPolicy = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <ul>
-        <li>Youtube Subscription Date does <strong> not </strong> collect your personal information. </li>
-        <li>Youtube Subscription Date ises Google Authentication <strong>only</strong> to retrieve your channel subscription date.</li>
+      <li>
+        {" "}
+        Youtube Subscription Date does <strong> not </strong> collect your
+        personal information.{" "}
+      </li>
+      <li>
+        Youtube Subscription Date ises Google Authentication{" "}
+        <strong>only</strong> to retrieve your channel subscription date.
+      </li>
     </ul>
   </Main>
 );
