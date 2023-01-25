0997c1734a6dc4480b06b09aee24e8a025db66c2
Sean Suzuki <suzatsuhiro@gmail.com>
2023-01-25 11:21:29 -0800

    Removed placeholder contents.

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -6,20 +6,9 @@ import { Main } from '@/templates/Main';
 const Blog = () => (
   <Main meta={<Meta title="Lorem ipsum" description="Lorem ipsum" />}>
     <p>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione fuga
-      recusandae quidem. Quaerat molestiae blanditiis doloremque possimus labore
-      voluptatibus distinctio recusandae autem esse explicabo molestias officia
-      placeat, accusamus aut saepe.
+      Coming Soon...
     </p>
 
-    {[...Array(10)].map((_, index) => (
-      <div
-        className="my-4 w-full rounded-md border-2 border-gray-400 px-2 py-1"
-        key={index}
-      >
-        <Link href={`/blog/blog-${index}`}>{`Blog - ${index}`}</Link>
-      </div>
-    ))}
   </Main>
 );
 
