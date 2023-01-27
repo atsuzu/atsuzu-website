99803737e05d3a65a3ca61ddffc2a3ee892e56ba
Sean Suzuki <suzatsuhiro@gmail.com>
2023-01-27 14:43:53 -0800

    Using Link and updated test.

--- src/pages.test/about.test.tsx
+++ src/pages.test/about.test.tsx
@@ -5,14 +5,12 @@ import About from '@/pages/about';
 // The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
 // The mock has been moved to `__mocks__` folder to avoid duplication
 
-describe('About page', () => {
-  describe('Render method', () => {
-    it('should have one paragraph of `What the application does`', () => {
+describe("About page", () => {
+  describe("Render method", () => {
+    it("should have one paragraph of `What the application does`", () => {
       render(<About />);
 
-      const paragraph = screen.queryByText(
-        /What the application does/
-      );
+      const paragraph = screen.queryByText(/What the application does/);
 
       expect(paragraph).toBeInTheDocument();
     });
